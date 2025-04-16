949c589526cc658b79db8fe1515a9759da92fd87
vitoriadf <vitoriadf23@gmail.com>
2025-04-15 22:36:30 -0300

    design da navbar

--- resources/views/components/responsive-nav-link.blade.php
+++ resources/views/components/responsive-nav-link.blade.php
@@ -2,8 +2,8 @@
 
 @php
 $classes = ($active ?? false)
-            ? 'block w-full ps-3 pe-4 py-2 border-l-4 border-indigo-400 dark:border-indigo-600 text-start text-base font-medium text-indigo-700 dark:text-indigo-300 bg-indigo-50 dark:bg-indigo-900/50 focus:outline-none focus:text-indigo-800 dark:focus:text-indigo-200 focus:bg-indigo-100 dark:focus:bg-indigo-900 focus:border-indigo-700 dark:focus:border-indigo-300 transition duration-150 ease-in-out'
-            : 'block w-full ps-3 pe-4 py-2 border-l-4 border-transparent text-start text-base font-medium text-gray-600 dark:text-fuchsia-900 hover:text-fuchsia-800 dark:hover:text-fuchsia-300 hover:bg-gray-50 dark:hover:bg-fuchsia-700 hover:border-gray-300 dark:hover:border-fuchsia-600 focus:outline-none focus:text-gray-800 dark:focus:text-fuchsia-200 focus:bg-gray-50 dark:focus:bg-fuchsia-700 focus:border-gray-300 dark:focus:border-fuchsia-600 transition duration-150 ease-in-out';
+            ? 'block w-full ps-3 pe-4 py-2 border-l-4 border-fuchsia-400 dark:border-fuchsia-600 text-start text-base font-medium text-fuchsia-700 dark:text-fuchsia-600 bg-fuchsia-50 dark:bg-fuchsia-300/50 focus:outline-none focus:text-indigo-800 dark:focus:text-indigo-200 focus:bg-indigo-100 dark:focus:bg-indigo-300 focus:border-indigo-700 dark:focus:border-fuchsia-300 transition duration-150 ease-in-out'
+            : 'block w-full ps-3 pe-4 py-2 border-l-4 border-transparent text-start text-base font-medium text-fchsia-600 dark:text-fuchsia-900 hover:text-fuchsia-800 dark:hover:text-fuchsia-300 hover:bg-gray-50 dark:hover:bg-fuchsia-700 hover:border-gray-300 dark:hover:border-fuchsia-600 focus:outline-none focus:text-fuchsia-800 dark:focus:text-fuchsia-200 focus:bg-gray-50 dark:focus:bg-fuchsia-400 focus:border-gray-300 dark:focus:border-fuchsia-600 transition duration-150 ease-in-out';
 @endphp
 
 <a {{ $attributes->merge(['class' => $classes]) }}>
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -134,7 +134,7 @@
             <div class="pt-4 pb-1 border-t border-gray-200 dark:border-gray-600">
                 <div class="px-4">
                     <div class="font-medium text-base text-gray-800 dark:text-fuchsia-900">{{ Auth::user()->name }}</div>
-                    <div class="font-medium text-sm text-gray-500">{{ Auth::user()->email }}</div>
+                    <div class="font-medium text-sm text-fuchsia-500">{{ Auth::user()->email }}</div>
                 </div>
 
                 <div class="mt-3 space-y-1">
