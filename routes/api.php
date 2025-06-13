966eb71e8d070ab46d07322cfe44437bc77aa5ed
vitoriadf <vitoriadf23@gmail.com>
2025-06-13 17:50:49 -0300

    implementamos o CRUD marcas

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ProdutoController;
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\MarcaController;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
